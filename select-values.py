3248220ced26c8cac3feaa748a71cd5aeeb132ca
aeberharhsr <19borki94@gmail.com>
2021-01-05 15:59:15 +0100

    beautify select statement

--- select-values.py
+++ select-values.py
@@ -7,11 +7,12 @@ db = pymysql.connect("192.168.178.33","sauna","1234","sauna" )
 cursor = db.cursor()
 
 # execute SQL query using execute() method.
-cursor.execute("SELECT VERSION()")
+cursor.execute("SELECT * FROM sensor_values")
 
 # Fetch a single row using fetchone() method.
-data = cursor.fetchone()
-print ("Database version : %s " % data)
+data = cursor.fetchall()
+for dat in data:
+    print(dat)
 
 # disconnect from server
 db.close()
